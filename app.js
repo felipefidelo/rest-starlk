1b1684ebad1b8aa5c9dd11d268cae6737515e5a6
Felipe Fidel <80488760+felipefidelo@users.noreply.github.com>
2023-11-25 10:09:36 -0300

    Atualizar o app.js

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3000; 
-
-
-
+const port = 3000; 
 app.get('/gif', (req, res) => {
    'anal/Starl.gif' 
-  const gifPath = 'anal/Starl.gif';
-
- 
+  const gifPath = 'anal/Starl.gif';
   res.sendFile(gifPath, { root: __dirname });
-});
-
-
+});
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
